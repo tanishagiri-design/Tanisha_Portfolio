478b697e010222c2327a676222c34c2eb46e6651
Tanisha Atmaram Giri <tgiri7566@gmail.com>
2026-02-13 14:41:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const cards = document.querySelectorAll('.card');
+
+window.addEventListener('scroll', () => {
+    cards.forEach(card => {
+        const position = card.getBoundingClientRect().top;
+        const screen = window.innerHeight;
+
+        if(position < screen - 100) {
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
